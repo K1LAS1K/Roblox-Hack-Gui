c6d79501aa97107d2aa24fef36f874854cc366c5
K1LAS1K <egekara303@gmail.com>
2025-05-10 17:58:35 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
--- KILASIK GUI - Main Loader Script
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part1"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part2"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part3"))()
