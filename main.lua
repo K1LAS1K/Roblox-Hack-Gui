687d8c2ca7a41e9c5996fdb6930a192be5d33969
K1LAS1K <egekara303@gmail.com>
2025-05-10 17:56:29 -0400

    Update and rename gui to main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+-- KILASIK GUI - Main Loader Script
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part1"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part3"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/K1LAS1K/Roblox-Hack-Gui/main/KILASIK_GUI_Part4"))()
